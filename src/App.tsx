2f1965dec7aeabf56fea2d1815eb1e38bc9ee489
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-05-10 10:22:05 +0530

    v5 relese

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,30 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from './pages/Login';
 import { Designer } from './pages/Designer';
+import { Landing } from './pages/Landing';
 import { StateProvider } from './store/StateProvider';
 import { AuthProvider } from './store/AuthProvider';
 import { useAuth } from './store/AuthProvider';
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { user } = useAuth();
-  return user ? <>{children}</> : <Navigate to="/" replace />;
+  return user ? <>{children}</> : <Navigate to="/login" replace />;
 };
 
 function App() {
   return (
-    <BrowserRouter  basename="/HCI">
+    <BrowserRouter   basename="/HCI">
       <AuthProvider>
         <StateProvider>
           <Routes>
-            <Route path="/" element={<Designer />} />
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
             <Route
               path="/designer"
               element={
-                // <ProtectedRoute>
-                  <Login/>
-                // </ProtectedRoute>
+                <ProtectedRoute>
+                  <Designer />
+                </ProtectedRoute>
               }
             />
           </Routes>
